c927b16a3d3d4c672851f02264aacbf649ccfacf
R34lfy <g.rogovtsev@gmail.com>
2026-09-26 00:11:38 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/dev/rental/photorent/PhotoRentApplication.java
@@ -0,0 +1,13 @@
+package dev.rental.photorent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rental/photorent/PhotoRentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rental.photorent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
